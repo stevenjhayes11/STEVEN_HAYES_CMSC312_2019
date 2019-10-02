f674c7d502e194f6bb340008f75bb8519090fa73
stevenjhayes11 <41926543+stevenjhayes11@users.noreply.github.com>
2019-10-02 11:33:55 -0400

    Comments, I/O waiting, and RNG
    Added comments to most of the code and some randomness to how long the processes go for, also made the I/O processes change state to waiting when they are incrementing instead of running

--- Part1RoundRobin.java
+++ Part1RoundRobin.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+/*
+ * This program simulates an operating system as defined in the project guidelines
+ * using a round robin scheduler
+ */
+
 public class Part1RoundRobin {
 
 	/*
@@ -11,23 +16,46 @@ public class Part1RoundRobin {
 	 * and execute processes based on this list
 	 */
 	protected static class Scheduler{
+		//List of processes
 		protected ArrayList<Process> processList;
+		
+		/*
+		 * Constructor, assigns process list to Scheduler's arrayList
+		 * @param list of processes
+		 */
 		protected Scheduler(ArrayList<Process> processList)
 		{
 			this.processList = processList;
 		}
+		/*
+		 * adds a process to the current list of processes
+		 * @param new process
+		 */
 		protected void addProcess(Process newProcess)
 		{
 			processList.add(newProcess);
 		}
+		/*
+		 * removes a process from the specified location
+		 * @param location of process to be removed
+		 */
 		protected void removeProcess(int index)
 		{
 			processList.remove(index);
 		}
+		/*
+		 * get process from specified location
+		 * @param location of process to be retrieved
+		 * @return process at that location
+		 */
 		protected Process getProcess(int index)
 		{
 			return processList.get(index);
 		}
+		/*
+		 * gets how many processes are in the array list
+		 * @return number of processes remaining
+		 */
 		protected int getSize()
 		{
 			return processList.size();
@@ -43,11 +71,17 @@ public class Part1RoundRobin {
 		//PCB DATA
 		int ID;
 		int state; //0 - ready, 1 - running, 2 - waiting
-		//DATA
-		int timeNeeded;
-		int currentTime;
+		int timeNeeded; //how many times a process will execute
+		int currentTime; //how many times a process has executed
 		String processType;
 		
+		/*
+		 * Constructor, creates all PCB data except currentTime, which is always initially 0
+		 * @param ID - number ID of process
+		 * @param state - current state of process
+		 * @param timeNeeded - how long to execute the process
+		 * @param processType - type of process
+		 */
 		protected Process(int ID, int state, int timeNeeded, String processType)
 		{
 			this.ID = ID;
@@ -56,40 +90,75 @@ public class Part1RoundRobin {
 			currentTime = 0;
 			this.processType = processType;
 		}
+		/*
+		 * sets the state of this process
+		 * @param state to be set to
+		 */
 		protected void setState(int state)
 		{
 			this.state = state;
 		}
+		/*
+		 * gets the state this process is in
+		 * @return state process is in
+		 */
 		protected int getState()
 		{
 			return state;
 		}
+		/*
+		 * sets process ID
+		 * @param ID to be set this to
+		 */
 		protected void setID(int ID)
 		{
 			this.ID = ID;
 		}
+		/*
+		 * gets process ID
+		 * @return this process ID
+		 */
 		protected int getID()
 		{
 			return ID;
 		}
+		/*
+		 * increments that of the number of times this has executed
+		 */
 		protected void incrementTime()
 		{
 			currentTime++;
 		}
+		/*
+		 * gets how many times this has executed
+		 * @return how many times it has executed
+		 */
 		protected int getCurrentTime()
 		{
 			return currentTime;
 		}
+		/*
+		 * gets how much time this needs to execute from start to finish,
+		 * independent of current execution time
+		 * @return time needed to finish
+		 */
 		protected int getTimeNeeded()
 		{
 			return timeNeeded;
 		}
+		/*
+		 * gets the type of process
+		 * @return this process type
+		 */
 		protected String getProcessType()
 		{
 			return processType;
 		}
 	}
 	
+	/*
+	 * Dispatcher only changes the state of processes
+	 */
 	protected static class Dispatcher{
 		protected Dispatcher()
 		{
@@ -119,22 +188,18 @@ public class Part1RoundRobin {
 		
 		Scheduler scheduler;
 		
-		//load in file
-		
-		//read header info
-		//---store Header info
 		String name;
 		int runtime;
 		int availMem;
 		Dispatcher dispatch = new Dispatcher();
 		
 		//read in processes and create PCB on intialization
-		//QUESTION - put all processes in then go through, correct?
 		ArrayList<Process> processList = new ArrayList<Process>(1);
 		File source = new File("C:\\Users\\steve\\Desktop\\VCU\\CMSC 312\\Test Files for Part 1\\TextProcessor1.txt");
 		
 		Scanner sc = new Scanner(source);
 		int ID = 0;
+		//while there are more lines remaining from the file, keep getting new data
 		while(sc.hasNextLine())
 		{
 			String temp = sc.nextLine();
@@ -142,6 +207,7 @@ public class Part1RoundRobin {
 			int timeNeeded;
 			String processType;
 			
+			//get process type
 			if(temp.contains("CALCULATE"))
 			{
 				processType = "CALCULATE";
@@ -162,32 +228,43 @@ public class Part1RoundRobin {
 				processType = "OUT";
 				temp = temp.substring(4);
 			}
-			timeNeeded = Integer.parseInt(temp);
 			
+			//get number of process executions then add a random between 1-25
+			timeNeeded = Integer.parseInt(temp);
+			Random rand = new Random();
+			int x = rand.nextInt(25);
+			timeNeeded = timeNeeded + x;
+			
+			//create a new process using parsed data and add it to the process List
 			Process newProcess = new Process(ID++, 0, timeNeeded, processType);
 			processList.add(newProcess);
 		}
+		
+		//add process list to scheduler
 		scheduler = new Scheduler(processList);
 		
 		
 		int currentProcess = 0;
 		 
+		
+		//as long as scheduler has items still in it
 		while(scheduler.getSize() != 0)
 		{
 		//execute currentProcess until complete or
   		//i == 49
 				boolean processRemoved = false;
+				//execute until process completes of i == 25
 				for(int i = 0; i < 25; i++)
 			  	{
-				 	//TODO need to initialize processList before this
-					//if the process has not been completed
+					//if the process has not been completed, execute next part
 					if(scheduler.getProcess(currentProcess).getCurrentTime() != scheduler.getProcess(currentProcess).getTimeNeeded())
 			  		{
 			  			scheduler.getProcess(currentProcess).incrementTime();
 			  			System.out.print(scheduler.getProcess(currentProcess).getID() + " ");
 			  			System.out.println(scheduler.getProcess(currentProcess).getProcessType() + " " + scheduler.getProcess(currentProcess).getCurrentTime());
 			  		}
-			  		//if process has been completed, remove it from scheduler list
+			  		//if process has been completed, remove it from scheduler list and
+					//attempt to go to next process
 			  		else
 			  		{
 			  			i = 25;
@@ -197,14 +274,16 @@ public class Part1RoundRobin {
 					
 			  		
 			  	}
-			//TODO change state of old process
-			System.out.println("first " + currentProcess);
+				
+			//if the process was completed, remove it from the list
 			if(processRemoved)
 			{
 				if(currentProcess >= scheduler.getSize())
 					currentProcess = 0;
 				processRemoved = false;
 			}
+			//if the process was not completed, change state to ready and
+			//move on to next process on the list or to beginning of list if at the end
 			else
 			{
 				dispatch.changeStateToReady(scheduler.getProcess(currentProcess));
@@ -212,24 +291,19 @@ public class Part1RoundRobin {
 			 	if(currentProcess >= processList.size())
 			  		currentProcess = 0;	
 			}
-			System.out.println("second " + currentProcess);
 			 
+			//as long as there are still processes remaining, change the new process
+			//to be worked's state on to "running"
 			if(scheduler.getSize() != 0)
 			{
-				dispatch.changeStateToRunning(scheduler.getProcess(currentProcess));
+				//If the process is I/O it goes to waiting, else goes to running
+				if(scheduler.getProcess(currentProcess).getProcessType().equalsIgnoreCase("I/O"))
+					dispatch.changeStateToWaiting(scheduler.getProcess(currentProcess));
+				else
+					dispatch.changeStateToRunning(scheduler.getProcess(currentProcess));
 			}
 					
 		}
-		
-		 
-		 
-		 
-		//start executing Processes
-		//---dispatcher starts at head of process list
-		//---gives a process time Q to execute
-		//---at end of time Q or when process is finished
-		//------stop or end process(move to ready or terminate)
-		//---move down list to next process unless process is exe
 		
 	}
 
