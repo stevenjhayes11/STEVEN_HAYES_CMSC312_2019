28d44abfc2c52e066c8eea5363c21961da37aa65
stevenjhayes11 <41926543+stevenjhayes11@users.noreply.github.com>
2019-11-10 16:39:01 -0500

    Last Commit, minor changes, comments
    Biggest thing here was adding comments, also added some additional printouts for more readability. Also realized that though I had made the Dispatcher, I had not used it anywhere, so I did that too. This will be last commit before submission

--- Part2_Round_Robin.java
+++ Part2_Round_Robin.java
@@ -9,7 +9,11 @@ import java.awt.event.*;
 
 public class Part2_Round_Robin {
 
-
+/*
+ * Thread objects are the individual commands taken from processes
+ * such as calculate, also hold the time required to execute
+ * a thread
+ */
 	protected static class Thread
 	{
 		private int cyclesNeeded;
@@ -18,7 +22,12 @@ public class Part2_Round_Robin {
 		String threadType;
 		private boolean isCritical;
 		
-		
+		/*
+		 * Constructor
+		 * parses out the thread info from a string
+		 * @param String representation of thread
+		 * @param ID of new thread
+		 */
 		protected Thread(String thread, int ID)
 		{
 			threadID = ID;
@@ -58,6 +67,16 @@ public class Part2_Round_Robin {
 			}
 			currentCycles = 0;
 		}
+		/*
+		 * Runs the thread through a single iteration of
+		 * its command
+		 * @return whether thread has completed all
+		 * cycles
+		 * TODO for next part of project
+		 * this iteration of runThread does not allow
+		 * for accurate tracking of run time due to critical
+		 * section handling
+		 */
 		protected boolean runThread()
 		{
 			
@@ -75,11 +94,27 @@ public class Part2_Round_Robin {
 				return false;
 			}
 		}
+		/*
+		 * @return if thread is critical/uninterruptible
+		 */
 		protected boolean isCrit()
 		{
 			return isCritical;
 		}
+		/*
+		 * @return type of thread i.e. calculate
+		 */
+		protected String getThreadType()
+		{
+			return threadType;
+		}
 	}
+	
+	/*
+	 * The Process object's main duty is to keep a list of
+	 * threads and to run those threads when called from a
+	 * scheduler
+	 */
 	protected static class Process
 	{
 		private int state;
@@ -90,14 +125,19 @@ public class Part2_Round_Robin {
 		private String processName;
 		private ArrayList<Thread> threads;
 		
-		protected Process()
-		{
-			
-		}
+		/*
+		 * Constructor
+		 * Brings in a scanner which contains the output from 
+		 * the template files and an integer ID
+		 * The scanner then is run through to extract PCB data
+		 * and individual Thread strings, which are then passed
+		 * to the thread class for further parsing
+		 */
 		protected Process(Scanner process, int processID)
 		{
 			
 			threads = new ArrayList<Thread>();
+			this.processID = processID;
 			//Extract PCB data
 			state = 0;
 			for(int i = 0; i < 4; i++)
@@ -140,59 +180,92 @@ public class Part2_Round_Robin {
 			}
 		}
 		/*
+		 * Sets the state of a process, only used in Dispatcher
 		 * state 0 is ready, 1 is running, 2 is waiting
+		 * @param new State
 		 */
 		protected void setState(int newState)
 		{
 			state = newState;
 		}
+		/*
+		 * @return current state
+		 */
 		protected int getState()
 		{
 			return state;
 		}
-		protected void setTimeNeeded(int timeNeeded)
-		{
-			this.timeNeeded = timeNeeded;
-		}
-		protected int getTimeNeeded()
-		{
-			return timeNeeded;
-		}
-		protected void setCurrentTime(int timeModifier)
-		{
-			currentTime = currentTime - timeModifier;
-		}
-		protected int getCurrentTime()
-		{
-			return currentTime;
-		}
+		/*
+		 * Sets memory needed by the process
+		 * @param memory needed by process
+		 */
 		protected void setMemRequired(int memRequired)
 		{
 			this.memRequired = memRequired;
 		}
+		/*
+		 * @return memory required by processor
+		 */
 		protected int getMemRequired()
 		{
 			return memRequired;
 		}
-		protected void setProcessName(String processName)
+		/*
+		 * @return the thread at the front of the
+		 * list of threads, which is the thread 
+		 * that will be executing at any given time
+		 */
+		protected Thread getcurrentThread()
 		{
-			this.processName = processName;
+			return threads.get(0);
 		}
-		protected String getProcessName()
-		{
-			return processName;
-		}
+		/*
+		 * @param thread to be added to thread list
+		 */
 		protected void addThread(Thread newThread)
 		{
 			threads.add(newThread);
 		}
+		/*
+		 * @return integer ID value for process
+		 */
+		protected int getProcessID()
+		{
+			return processID;
+		}
+		/*
+		 * runProcess's main duty is to call the runThread function
+		 * it first checks if the current thread is EXE and alone,
+		 * if so it will recognize the process has been completed
+		 * If it is not an EXE thread, then it will run a thread 
+		 * either maxTime times or, if the thread is critical, until
+		 * the thread has completed
+		 * @param maximum number of times to runThread
+		 * @return number of cycles taken to run
+		 */
 		protected int runProcess(int maxTime)
 		{
+			if(threads.get(0).getThreadType().equalsIgnoreCase("EXE"))
+			{	
+				if(threads.size() == 1)
+				{	
+					System.out.println(this.processID + " EXE");
+					System.out.println("PROCESS ENDED");
+					threads.remove(0);
+					return 1;
+				}
+				else
+				{
+					Thread tempThread = threads.remove(0);
+					threads.add(tempThread);
+				}
+			}
 			int count = 0;
 			boolean threadComplete = false;
 			//Run the thread until the max amount of cycles is reached or if its crit
 			while((count < maxTime && !threadComplete) || (threads.get(0).isCrit() && !threadComplete))
 			{
+				System.out.print("Process ID: " + getProcessID() + " ");
 				threadComplete = threads.get(0).runThread();
 				count++;
 			}
@@ -205,12 +278,22 @@ public class Part2_Round_Robin {
 			}
 			return count;
 		}
+		/*
+		 * @return number of threads in this process
+		 */
 		protected int getSize()
 		{
 			return threads.size();
 		}
 
 	}
+	/*
+	 * The scheduler object is primarily used as a memory location
+	 * and to keep track of how many processes are remaining, and
+	 * what state they are in. It's other primary function is to call the 
+	 * executeCycle function, which then calls the runProcess function
+	 * and changes the states of the relevant processes
+	 */
 	protected static class Scheduler
 	{
 		private int totalMem;
@@ -218,51 +301,102 @@ public class Part2_Round_Robin {
 		private ArrayList<Process> processList;
 		private Dispatcher mainDispatch;
 		
+		/*
+		 * Constructor
+		 * @param total memory this scheduler has to use
+		 */
 		protected Scheduler(int totalMem)
 		{
 			this.totalMem = totalMem;
 			availMem = totalMem;
 			processList = new ArrayList<Process>();
+			mainDispatch = new Dispatcher();
 		}
+		/*
+		 * Constructor
+		 * @param total memory to use
+		 * @param list of processes
+		 */
 		protected Scheduler(int totalMem, ArrayList<Process> processList)
 		{
 			processList = new ArrayList<Process>(); 
 			this.totalMem = totalMem;
 			availMem = totalMem;
 			this.processList = processList;
+			mainDispatch = new Dispatcher();
 		}	
+		/*
+		 * change available memory
+		 * @param amount to be changed by
+		 */
 		protected void modifyMem(int memModifier)
 		{
 			this.availMem = availMem + memModifier;
 		}
+		/*
+		 * @return available memory
+		 */
 		protected int getAvailMem()
 		{
 			return availMem;
 		}
+		/*
+		 * removes a process from the process array list
+		 * @param position of process to be removed
+		 */
 		protected void removeProcess(int processPos)
 		{
 			this.modifyMem(processList.get(processPos).getMemRequired());
 			processList.remove(processPos);
 		}
+		/*
+		 * adds a process to the end of the list
+		 * @param process to be added
+		 */
 		protected void addProcess(Process newProcess)
 		{
 			processList.add(newProcess);
 			this.modifyMem(-(newProcess.getMemRequired()));
 		}
+		/*
+		 * @param list position
+		 * @return process at processPos
+		 */
 		protected Process getProcess(int processPos)
 		{
 			return processList.get(processPos);
 		}
+		/*
+		 * @return process array list size
+		 */
 		protected int getSize()
 		{
 			return processList.size();
 		}
+		/*
+		 * changes states of processes as they are being executed 
+		 * and calls the runProcess cycle
+		 * @param time allocated for a non critical process to run
+		 * @return how many cycles were needed
+		 */
 		protected int executeCycle(int cycleTime)
 		{
+			if(processList.get(0).getcurrentThread().getThreadType().equalsIgnoreCase("Calculate"))
+				mainDispatch.changeStateToRunning(processList.get(0));
+			else if(processList.get(0).getcurrentThread().getThreadType().equalsIgnoreCase("I/O"))
+				mainDispatch.changeStateToWaiting(processList.get(0));
 			int executionTime = processList.get(0).runProcess(cycleTime);
+			mainDispatch.changeStateToReady(processList.get(0));
 			return executionTime;
 		}
 	}
+	
+	/*
+	 * Dispatcher class only changes the states of processes
+	 * 0 is ready
+	 * 1 is running
+	 * 2 is waiting
+	 */
 	protected static class Dispatcher{
 		protected Dispatcher()
 		{
@@ -285,6 +419,7 @@ public class Part2_Round_Robin {
 	}
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		//initialize memory
 		Scheduler schedulerRAM = new Scheduler(150);
 		Scheduler schedulerHDD = new Scheduler(100000);
 		int cycleLength = 25;
@@ -297,9 +432,9 @@ public class Part2_Round_Robin {
 		int test3 = kb.nextInt();
 		int test4 = kb.nextInt();
 		
-		//gets a certain number of repitions from each file template
+		//gets a certain number of repetitions from each file template
 		//and stores them in either the RAM scheduler or HDD scheduler
-		int currentID = 0;
+		int currentID = 1;
 		for(int i = 0; i < 4; i++)
 		{
 			int reps;
@@ -340,7 +475,10 @@ public class Part2_Round_Robin {
 					sc = new Scanner(source5);
 					break;
 				}
-				Process newProcess = new Process(sc, currentID++);
+				System.out.println("currentID: " + currentID);
+				Process newProcess = new Process(sc, currentID);
+				System.out.println("New Process ID: " + newProcess.getProcessID());
+				currentID++;
 				if(schedulerRAM.getAvailMem() > newProcess.getMemRequired())
 				{
 					schedulerRAM.addProcess(newProcess);
@@ -348,6 +486,7 @@ public class Part2_Round_Robin {
 				else
 				{
 					schedulerHDD.addProcess(newProcess);
+					System.out.println("added to HDD");
 				}	
 			}
 		}
@@ -358,6 +497,8 @@ public class Part2_Round_Robin {
 
 		while(schedulerRAM.getSize() > 0)
 		{
+			//if the current process in ram still has threads remaining
+			//then execute a cycle
 			if(schedulerRAM.getProcess(0).getSize() != 0)
 			{
 				int cyclesRun = schedulerRAM.executeCycle(cycleLength);
@@ -366,11 +507,12 @@ public class Part2_Round_Robin {
 				schedulerRAM.removeProcess(0);
 				schedulerRAM.addProcess(temp);
 			}
+			//else remove the process, check and see if a new process
+			//from the HDD can be inserted into the RAM
 			else
 			{
+				System.out.println("Process " + schedulerRAM.getProcess(0).getProcessID() + " Removed post completion");
 				schedulerRAM.removeProcess(0);
-				System.out.println("Process Removed");
-				
 				if(schedulerHDD.getSize() != 0)
 				{
 					if(schedulerHDD.getProcess(0).getMemRequired() < schedulerRAM.getAvailMem())
@@ -383,7 +525,6 @@ public class Part2_Round_Robin {
 				}
 			}
 				
-		//TODO add switching from HDD memory to RAM memory checks	
 		}
 	}
 	
